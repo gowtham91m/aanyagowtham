d8573d8af5a6e225d2dd8d0febf9ef2818ee0135
ggwm <ggwm@amazon.com>
2024-06-27 17:03:32 -0700

    formatting update

--- src/Birthday/Birthday.tsx
+++ src/Birthday/Birthday.tsx
@@ -1,6 +1,6 @@
 import { AddToCalendarButton } from "add-to-calendar-button-react";
 import Countdown from "react-countdown";
-import aanya from './assets/aanya.png'
+import aanya from "./assets/aanya.png";
 
 export const Birthday = () => {
   const backgroundImage = require("./assets/background.jpg"); // Assuming the image is in your project
@@ -24,7 +24,7 @@ export const Birthday = () => {
         height: "100vh",
         width: "100vw",
         color: "black", // Text color
-        filter: 'brightness(0.7)',
+        filter: "brightness(0.7)",
       }}
     >
       <div
@@ -44,14 +44,15 @@ export const Birthday = () => {
             renderer={renderer}
           />
         </div>
+        <h4>It's me, Aanya! I'm turning one on 07/14/2024</h4>
         <h4>
-        It's me, Aanya! I'm turning one on 07/14/2024
+          My mommy and daddy are throwing me a party with cake, balloons, and
+          all sorts of fun stuff! I can't wait to play with you and show you my
+          new toys.
         </h4>
         <h4>
-
-My mommy and daddy are throwing me a party with cake, balloons, and all sorts of fun stuff! I can't wait to play with you and show you my new toys.
-
-Please RSVP so my mommy can make sure there's enough cake for everyone!
+          Please RSVP so my mommy can make sure there's enough cake for
+          everyone!
         </h4>
 
         <h4>Sunday, July 14th at 11:00 am</h4>
@@ -77,9 +78,7 @@ Please RSVP so my mommy can make sure there's enough cake for everyone!
           ></AddToCalendarButton>
         </div>
 
-        <h4>
-        Insignia By Bosa, 588 Bell St, Seattle, WA 98121
-        </h4>
+        <h4>Insignia By Bosa, 588 Bell St, Seattle, WA 98121</h4>
 
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.488640386752!2d-122.34536762297512!3d47.61663207119084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5490154bd92b6e21%3A0x978a2146d70dee3f!2sInsignia%20By%20Bosa!5e0!3m2!1sen!2sus!4v1719506138555!5m2!1sen!2sus"
@@ -90,21 +89,23 @@ Please RSVP so my mommy can make sure there's enough cake for everyone!
       </div>
     </div>
   );
-
 };
 
-export const LinkButton = ({ href, children }:any) => {
+export const LinkButton = ({ href, children }: any) => {
   return (
-    <a href={href} style={{
-      // Add button styles here (e.g., padding, background-color, border)
-      display: 'inline-block',
-      padding: '10px 20px',
-      backgroundColor: '#4CAF50',
-      color: 'white',
-      borderRadius: '5px',
-      cursor: 'pointer',
-      textDecoration: 'none', // Remove underline from link
-    }}>
+    <a
+      href={href}
+      style={{
+        // Add button styles here (e.g., padding, background-color, border)
+        display: "inline-block",
+        padding: "10px 20px",
+        backgroundColor: "#4CAF50",
+        color: "white",
+        borderRadius: "5px",
+        cursor: "pointer",
+        textDecoration: "none", // Remove underline from link
+      }}
+    >
       {children}
     </a>
   );
